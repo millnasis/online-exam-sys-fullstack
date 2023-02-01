5c79f49c78481b402cc6ef1b5a3ff65a92d74372
millnasis <1985551393@qq.com>
2023-02-01 21:35:36 +0900

    首次提交

---
+++ src/main/java/com/online_exam_sys/OnlineExamSysApplication.java
@@ -0,0 +1,13 @@
+package com.online_exam_sys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineExamSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineExamSysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/online_exam_sys/OnlineExamSysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.online_exam_sys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineExamSysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
