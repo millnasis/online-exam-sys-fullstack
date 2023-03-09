c5bd119f65f2fe68f78148dbb384fadf74157726
millnasis <1985551393@qq.com>
2023-03-09 10:49:04 +0900

    学生信息页编辑

--- front/src/student/components/userSetting/userSetting.jsx
+++ front/src/student/components/userSetting/userSetting.jsx
@@ -1,11 +1,19 @@
-import { Avatar, Divider } from "antd";
+import { Avatar, Button, Divider, Input, Radio, Upload } from "antd";
 import React from "react";
 import { connect } from "react-redux";
+import ImgCrop from "antd-img-crop";
+import { CameraOutlined } from "@ant-design/icons";
 import "./userSetting.scss";
 
 class UserSetting extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      edit: false,
+      form: { sex: "F" },
+      avatar: [],
+    };
   }
 
   render() {
@@ -21,28 +29,99 @@ class UserSetting extends React.Component {
               src={userInfo.st_avatar}
               className="user-avatar"
             ></Avatar>
+            {this.state.edit ? (
+              <ImgCrop rotate>
+                <Upload
+                  className="user-avatar-upload-btn"
+                  action="/api/upload/avatar"
+                  listType="picture-card"
+                  fileList={this.state.avatar}
+                  name="avatar"
+                  onChange={(e) => {
+                    this.setState({ avatar: e.fileList });
+                  }}
+                  maxCount={1}
+                >
+                  <CameraOutlined></CameraOutlined>上传
+                </Upload>
+              </ImgCrop>
+            ) : null}
           </div>
           <div className="user-info-detail">
-            <p className="user-name">{userInfo.st_name}</p>
+            <p className="user-name">
+              {this.state.edit ? (
+                <Input className="input-bar" size="large"></Input>
+              ) : (
+                userInfo.st_name
+              )}
+            </p>
+            <p
+              className="user-info-change-btn"
+              onClick={(e) => this.setState({ edit: true })}
+            >
+              编辑信息
+            </p>
             <p>
               <span>学号</span>
-              {userInfo.st_card}
+              {this.state.edit ? (
+                <Input className="input-bar"></Input>
+              ) : (
+                userInfo.st_card
+              )}
             </p>
             <Divider></Divider>
             <p>
               <span>性别</span>
-              {userInfo.st_sex === "M" ? "男" : "女"}
+              {this.state.edit ? (
+                <span className="input-bar radio-group">
+                  <input
+                    type={"radio"}
+                    name="sex"
+                    onClick={() =>
+                      this.setState({ form: { ...this.state.form, sex: "F" } })
+                    }
+                    checked={this.state.form.sex === "F"}
+                    readOnly
+                  ></input>
+                  男&nbsp;
+                  <input
+                    readOnly
+                    type={"radio"}
+                    name="sex"
+                    onClick={() =>
+                      this.setState({ form: { ...this.state.form, sex: "M" } })
+                    }
+                    checked={this.state.form.sex === "M"}
+                  ></input>
+                  女
+                </span>
+              ) : userInfo.st_sex === "M" ? (
+                "男"
+              ) : (
+                "女"
+              )}
             </p>
             <Divider></Divider>
             <p>
               <span>年龄</span>
-              {userInfo.st_age}
+              {this.state.edit ? (
+                <Input className="input-bar"></Input>
+              ) : (
+                userInfo.st_age
+              )}
             </p>
             <Divider></Divider>
             <p>
               <span>注册日期</span>
               {userInfo.st_registerdate}
             </p>
+            {!this.state.edit &&
+            (
+              <p>
+                <Button>取消</Button>
+                <Button type="primary">保存</Button>
+              </p>
+            )}
           </div>
         </div>
       </div>
